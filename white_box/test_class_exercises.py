097bcda2b9a4dd9c71502d118b9b4557f289ebbf
Sara Aranda <131466857+saranda44@users.noreply.github.com>
2025-09-24 11:45:59 -0600

    chore: Agregar respuestas de ejercicios 1 a 3 (#6)
    * chore: Agregar respuestas de ejercicios 1 a 3
    
    Agregar respuestas a ejercicios de test white box del 1 al 3
    
    * fix: Arreglar pre-commit
    
    Arreglar pre-commit
    
    * fix: borrar importacion no usada
    
    borrar importacion de una funcion que no estaba usando
    
    * fix: Agregar step en unit-test.yml
    
    agregar step en unit-test.yml temporal solo para que si corra

---
+++ white_box/test_class_exercises.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+
+"""
+White-box unit testing examples.
+"""
+import unittest
+
+from white_box.class_exercises import (
+    VendingMachine,
+    divide,
+    get_grade,
+    is_even,
+    is_triangle,
+)
+
+
+class TestWhiteBox(unittest.TestCase):
+    """
+    White-box unittest class.
+    """
+
+    def test_is_even_with_even_number(self):
+        """
+        Checks if a number is even.
+        """
+        self.assertTrue(is_even(0))
+
+    def test_is_even_with_odd_number(self):
+        """
+        Checks if a number is not even.
+        """
+        self.assertFalse(is_even(7))
+
+    def test_divide_by_non_zero(self):
+        """
+        Checks the divide function works as expected.
+        """
+        self.assertEqual(divide(10, 2), 5)
+
+    def test_divide_by_zero(self):
+        """
+        Checks the divide function returns 0 when dividing by 0.
+        """
+        self.assertEqual(divide(10, 0), 0)
+
+    def test_get_grade_a(self):
+        """
+        Checks A grade.
+        """
+        self.assertEqual(get_grade(95), "A")
+
+    def test_get_grade_b(self):
+        """
+        Checks B grade.
+        """
+        self.assertEqual(get_grade(85), "B")
+
+    def test_get_grade_c(self):
+        """
+        Checks C grade.
+        """
+        self.assertEqual(get_grade(75), "C")
+
+    def test_get_grade_f(self):
+        """
+        Checks F grade.
+        """
+        self.assertEqual(get_grade(65), "F")
+
+    def test_is_triangle_yes(self):
+        """
+        Checks the three inputs can form a triangle.
+        """
+        self.assertEqual(is_triangle(3, 4, 5), "Yes, it's a triangle!")
+
+    def test_is_triangle_no_1(self):
+        """
+        Checks the three inputs can't form a triangle when C is greater or equal than A + B.
+        """
+        self.assertEqual(is_triangle(3, 4, 7), "No, it's not a triangle.")
+
+    def test_is_triangle_no_2(self):
+        """
+        Checks the three inputs can't form a triangle when B is greater or equal than A + C.
+        """
+        self.assertEqual(is_triangle(2, 3, 1), "No, it's not a triangle.")
+
+    def test_is_triangle_no_3(self):
+        """
+        Checks the three inputs can't form a triangle when A is greater or equal than B + C.
+        """
+        self.assertEqual(is_triangle(2, 1, 1), "No, it's not a triangle.")
+
+
+class TestWhiteBoxVendingMachine(unittest.TestCase):
+    """
+    Vending Machine unit tests.
+    """
+
+    # @classmethod
+    # def setUpClass(cls):
+    #    return
+
+    def setUp(self):
+        self.vending_machine = VendingMachine()
+        self.assertEqual(self.vending_machine.state, "Ready")
+
+    # def tearDown(self):
+    #    return
+
+    # @classmethod
+    # def tearDownClass(cls):
+    #    return
+
+    def test_vending_machine_insert_coin_error(self):
+        """
+        Checks the vending machine can accept coins.
+        """
+        self.vending_machine.state = "Dispensing"
+
+        output = self.vending_machine.insert_coin()
+
+        self.assertEqual(self.vending_machine.state, "Dispensing")
+        self.assertEqual(output, "Invalid operation in current state.")
+
+    def test_vending_machine_insert_coin_success(self):
+        """
+        Checks the vending machine fails to accept coins when it's not ready.
+        """
+        output = self.vending_machine.insert_coin()
+
+        self.assertEqual(self.vending_machine.state, "Dispensing")
+        self.assertEqual(output, "Coin Inserted. Select your drink.")
