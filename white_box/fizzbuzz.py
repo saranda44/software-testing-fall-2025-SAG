8291727cf6ddb17ed50f27fb813ae4048a04ab2a
sara <sara.aranda.gtz@gmail.com>
2025-10-20 12:38:12 -0600

    chore: iteracion 2
    Modificar fizzbuzz para test 1

--- white_box/fizzbuzz.py
+++ white_box/fizzbuzz.py
@@ -3,5 +3,8 @@
 
 
 def fizzbuzz(number):
-    """funcion fizzbuzz implementacion: iteracion 1"""
-    return number
+    """funcion fizzbuzz iteracion 2: return string number"""
+    result = ""
+    if number % 3 != 0 and number % 5 != 0:
+        result = str(number)
+    return result
