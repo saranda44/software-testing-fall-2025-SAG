b2c1da8a20819e6f842ee732454392d3320a9476
sara <sara.aranda.gtz@gmail.com>
2025-10-22 12:30:52 -0600

    chore: Version 3
    modificar add_v3 para que pase los requerimientos 3

--- white_box/string_calculator.py
+++ white_box/string_calculator.py
@@ -32,8 +32,18 @@ def add_v2(string_number):
 
 
 def add_v3(string_number):
-    """version dummy"""
-    return string_number
+    """version 3"""
+    if string_number == "":
+        return 0
+
+    if ",\n" in string_number or "\n," in string_number:
+        raise ValueError("Separadores inválidos mezclados")
+
+    # Reemplazar saltos de línea por comas para unificar
+    numbers = string_number.replace("\n", ",")
+    parts = numbers.split(",")
+
+    return sum(int(num) for num in parts if num)
 
 
 def add_v4(string_number):
