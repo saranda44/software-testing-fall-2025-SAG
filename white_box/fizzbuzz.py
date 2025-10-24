e6986a80bde66944630c816016888d54dcdff899
Sara Aranda <131466857+saranda44@users.noreply.github.com>
2025-10-24 11:39:59 -0600

    merge: agregar versiones de fizzbuzz (#12)
    * chore: Version 1
    
    Agregar test y funcion que resuleva el requerimiento 1
    
    * chore: Version 2
    
    Modificar funcion add_v2 para que cumpla requerimiento 2
    
    * chore: Version 3
    
    modificar add_v3 para que pase los requerimientos 3
    
    * chore: Version 4
    
    Modificar add_v4 par que pase los requerimientos 4
    
    * chore: Version 5
    
    Modificar add_v5 para que pasen los requerimientos 5, tambien cambiar
    nombre de las funciones a fromato should when
    
    * chore: Version 6
    
    modificar add_6 para que pasen los requerimientos 6. modificar funciones
    pasadas para que cumplan las otras reglas
    
    * chore: version 7
    
    Modificar funcion para que pase los test del requeirmeinto 1 a 7
    
    * chore: version 8
    
    Modificar funcion para que cumpla con todos los requerimientos
    
    * feat: agregar versiones de fizzbuzz
    
    Agregar las versiones anteriores de funcion fizzbuzz para entrega de
    tarefeat: agregar versiones de fizzbuzz
    
    Agregar las versiones anteriores de funcion fizzbuzz para entrega de
    tareaa

--- white_box/fizzbuzz.py
+++ white_box/fizzbuzz.py
@@ -2,8 +2,65 @@
 """Funcion fizzbuzz"""
 
 
-def fizzbuzz(number):
+def fizzbuzz_v1(number):
+    """funcion fizzbuzz iteracion 1"""
+    return number
+
+
+def fizzbuzz_v2(number):
+    """funcion fizzbuzz iteracion 2: return string number"""
+    result = ""
+    if number % 3 != 0 and number % 5 != 0:
+        result = str(number)
+    return result
+
+
+def fizzbuzz_v3(number):
     """funcion fizzbuzz iteracion 3: return "Fizz" cuando el numero es multiplo de 3"""
+    result = ""
+    if number % 3 != 0 and number % 5 != 0:
+        result = str(number)
+
+    if number % 3 == 0:
+        result = "Fizz"
+    return result
+
+
+def fizzbuzz_v4(number):
+    """funcion fizzbuzz iteracion 4: return "Buzz" cuando el numero es multiplo de 5"""
+    result = ""
+    if number % 3 != 0 and number % 5 != 0:
+        result = str(number)
+
+    if number % 3 == 0:
+        result = "Fizz"
+
+    if number % 5 == 0:
+        result = "Buzz"
+
+    return result
+
+
+def fizzbuzz_v5(number):
+    """funcion fizzbuzz iteracion 5: return "FizzBuzz" cuando el numero es multiplo de 3 y 5"""
+    result = ""
+    if number % 3 != 0 and number % 5 != 0:
+        result = str(number)
+
+    if number % 3 == 0:
+        result = "Fizz"
+
+    if number % 5 == 0:
+        result = "Buzz"
+
+    if number % 3 == 0 and number % 5 == 0:
+        result = "FizzBuzz"
+
+    return result
+
+
+def fizzbuzz(number):
+    """funcion fizzbuzz iteracion 6"""
     result = ""
     if number % 3 == 0:
         result = "Fizz"
