9fd215374dae9976443f31a93de7a1a6ff2c8592
sara <sara.aranda.gtz@gmail.com>
2025-10-27 19:08:32 -0600

    feat: version final
    funcion cumple todos los requerimientos

--- white_box/barcode_scanner.py
+++ white_box/barcode_scanner.py
@@ -9,14 +9,24 @@ PRICES = {
 
 def barcode_scanner(barcode, command=None):
     """
-    function barcode_scanner
+    function barcode_sacanner
     """
     total_print = ""
-    if command is None and barcode in PRICES:
+    if command == "total":
+        if not barcode:
+            return "Error: empty list of barcodes"
+
+        total = 0
+        for code in barcode:
+            if code not in PRICES:
+                return "Error: barcode not found"
+            total += PRICES[code]
+            total_print = f"${total:.2f}"
+    else:
+        if barcode == "":
+            return "Error: empty barcode"
+        if barcode not in PRICES:
+            return "Error: barcode not found"
         total_print = f"${PRICES[barcode]:.2f}"
-    if barcode not in PRICES:
-        total_print = "Error: barcode not found"
-    if barcode == "":
-        total_print = "Error: empty barcode"
 
     return total_print
