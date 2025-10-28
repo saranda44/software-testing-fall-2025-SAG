3fbf638709574845b2e5325c6a9897888e7cbae5
sara <sara.aranda.gtz@gmail.com>
2025-10-27 19:00:46 -0600

    feat: version 2
    version dos de funcion para requerimientos 1 y 2

--- white_box/barcode_scanner.py
+++ white_box/barcode_scanner.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 """function barcode_scanner"""
 
+PRICES = {
+    "12345": 7.25,
+    "23456": 12.50,
+}
+
 
 def barcode_scanner(barcode, command=None):
     """
     function barcode_scanner
     """
     total_print = ""
-    if command is None and barcode == "12345":
-        total_print = "$7.25"
+    if command is None:
+        total_print = f"${PRICES[barcode]:.2f}"
     return total_print
