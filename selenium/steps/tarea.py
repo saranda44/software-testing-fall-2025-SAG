c40d0bb0d79d39321e5763bf60415cac94d3a8ae
sara <sara.aranda.gtz@gmail.com>
2025-11-14 18:51:49 -0600

    fix: Agregar headless otra vez
    Agregar headlesss otra vez

--- selenium/steps/tarea.py
+++ selenium/steps/tarea.py
@@ -17,6 +17,9 @@ from selenium import webdriver
 def step_open_google(context):
     """Opens Google in Chrome."""
     options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-blink-features=AutomationControlled")
     context.driver = webdriver.Chrome(options=options)
     context.driver.get("https://www.google.com")
